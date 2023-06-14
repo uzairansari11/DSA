0264c5078b02fabd5d24564e88b5127da9813959
uzairansari11 <uzairans532@gmail.com>
2023-06-14 21:15:13 +0530

    Remove Duplicates

--- TwoPointers/RemoveDuplicate.js
+++ TwoPointers/RemoveDuplicate.js
@@ -1,1 +1,25 @@
 let arr = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+
+let j = 0
+
+for (let i = 0; i < arr.length - 1; i++) {
+
+    if (arr[i] !== arr[i + 1]) {
+        arr[j] = arr[i]
+        j++
+    }
+}
+
+arr[j] = arr[arr.length - 1]
+j++
+
+for (let i = 0; i < j; i++) {
+    console.log(arr[i])
+}
+
+/* 
+
+Time Complexcity O(n)
+Space Complexcity O(1)
+
+*/
